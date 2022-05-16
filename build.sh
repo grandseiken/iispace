6aa951e449774ef956daa51e73965911d635fe6c
stu <s+github@stu.scot>
2022-05-16 03:09:50 +0100

    remove old stuff

--- build.sh
+++
@@ -1,4 +0,0 @@
-cd "${0%/*}"
-git submodule update --init
-mkdir -p ./build
-(cd ./build && cmake .. -DCMAKE_BUILD_TYPE=Release && make -j 4 $@)
--- debug.sh
+++
@@ -1,4 +0,0 @@
-cd "${0%/*}"
-git submodule update --init
-mkdir -p ./debug
-(cd ./debug && cmake .. -DCMAKE_BUILD_TYPE=Debug && make -j 4 $@)
