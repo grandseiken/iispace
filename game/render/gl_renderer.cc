af87cb802fc76bb5b6746653ec855fd9d7274479
stu <s+github@stu.scot>
2022-05-19 23:42:25 +0100

    simplify shaders

--- game/render/gl_renderer.cc
+++ game/render/gl_renderer.cc
@@ -4,8 +4,24 @@
 #include "game/render/gl/program.h"
 #include "game/render/gl/texture.h"
 #include "game/render/gl/types.h"
-#include "game/render/shaders/shaders.h"
 #include <GL/gl3w.h>
+#include <nonstd/span.hpp>
+
+namespace {
+#include "game/render/shaders/rect.f.glsl.h"
+#include "game/render/shaders/rect.v.glsl.h"
+#include "game/render/shaders/text.f.glsl.h"
+#include "game/render/shaders/text.v.glsl.h"
+
+const nonstd::span<const std::uint8_t> kRectFragmentShader = {game_render_shaders_rect_f_glsl,
+                                                              game_render_shaders_rect_f_glsl_len};
+const nonstd::span<const std::uint8_t> kRectVertexShader = {game_render_shaders_rect_v_glsl,
+                                                            game_render_shaders_rect_v_glsl_len};
+const nonstd::span<const std::uint8_t> kTextFragmentShader = {game_render_shaders_text_f_glsl,
+                                                              game_render_shaders_text_f_glsl_len};
+const nonstd::span<const std::uint8_t> kTetVertexShader = {game_render_shaders_text_v_glsl,
+                                                           game_render_shaders_text_v_glsl_len};
+}  // namespace
 
 namespace ii {
 
